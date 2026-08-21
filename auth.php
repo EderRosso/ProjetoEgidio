904f67da2277c63229ec064b1a088483704ca74f
EderRosso <ederderosso@rede.ulbra.br>
2026-08-21 17:16:00 -0300

    Fix password hash

--- auth.php
+++ auth.php
@@ -4,7 +4,7 @@ session_start();
 // Configurações de Segurança
 define('ADMIN_USER', 'admin');
 // Hash SHA-256 de "BuffonAdmin!2026"
-define('ADMIN_PASS_HASH', 'b7804473e318f77af456c6d042129e96f1d9313a9de0cc473d09a800d072e9c2'); 
+define('ADMIN_PASS_HASH', 'c9639d0b07319d01ec017e1d23a0da085497d6d43bd7dcad2b515d404bd961a0'); 
 
 function checkAuth() {
     if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
